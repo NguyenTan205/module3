b6435c6506d0612c87e7e7e41b2b425ef826e172
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-08-29 23:08:12 +0700

    Quản lý User

---
+++ user-manager/src/main/java/org/example/usermanager/controller/UserServlet.java
@@ -0,0 +1,120 @@
+package org.example.usermanager.controller;
+
+import org.example.usermanager.model.User;
+import org.example.usermanager.service.UserDAO;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "UserServlet", urlPatterns = "/users")
+public class UserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UserDAO userDAO;
+
+    public void init() {
+        userDAO = new UserDAO();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "create":
+                    insertUser(req, resp);
+                    break;
+                case "edit":
+                    updateUser(req, resp);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "create":
+                    showNewForm(req, resp);
+                    break;
+                case "edit":
+                    showEditForm(req, resp);
+                    break;
+                case "delete":
+                    deleteUser(req, resp);
+                    break;
+                default:
+                    listUser(req, resp);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void listUser(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> listUser = userDAO.selectAllUsers();
+        req.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("user/list.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    private void showEditForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        User existingUser = userDAO.selectUser(id);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("user/edit.jsp");
+        req.setAttribute("user", existingUser);
+        dispatcher.forward(req, resp);
+    }
+
+    private void showNewForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("user/create.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    private void insertUser(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String country = req.getParameter("country");
+        User newUser = new User(name, email, country);
+        userDAO.insertUser(newUser);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("user/create.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    private void updateUser(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String country = req.getParameter("country");
+
+        User book = new User(id, name, email, country);
+        userDAO.updateUser(book);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("user/edit.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    private void deleteUser(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, SQLException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        userDAO.deleteUser(id);
+
+        List<User> listUser = userDAO.selectAllUsers();
+        req.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("user/list.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
